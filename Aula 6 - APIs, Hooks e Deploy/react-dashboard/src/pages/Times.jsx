acdfcac955d0931b0fc7e5250629711b9551ea5b
Ana Luiza <analu.tibi@Outlook.com>
2025-09-18 20:46:50 -0300

    API POKEMON

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Times.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Times.jsx
@@ -1,19 +1,14 @@
 import { useEffect, useState } from "react";
-
-const [times, setTimes] = useState([]);
-
+ 
 export default function Times() {
+  //const [times, setTimes] = useState([]);
   useEffect(() => {
-    fetch("https://v3.football.api-sports.io/teams?league=74&season=2023", {
-      headers: {
-        "x-apisports-key": "93661f9333b69bfe5b5ed3712546e190",
-      },
-    })
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
       .then((response) => response.json())
-      .then((data) => console.log(data.response))
-      .catch((erro) => console.error("Erro ao buscar times:", erro));
+      .then((data) => console.log(data))
+      .catch((erro) => console.error("Erro ao buscar times: ", erro));
   }, []);
-
+ 
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Times</h1>
